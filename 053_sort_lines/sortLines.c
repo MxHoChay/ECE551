b1d212f1c7b5b3ad10d49995abf768c8d876f3e8
jc977 <jc977@duke.edu>
2022-10-03 12:18:59 -0400

    assignment 053 1st try

--- 053_sort_lines/sortLines.c
+++ 053_sort_lines/sortLines.c
@@ -15,7 +15,54 @@ void sortData(char ** data, size_t count) {
 }
 
 int main(int argc, char ** argv) {
-  //WRITE YOUR CODE HERE!
-
+  char ** lines = NULL;
+  char * line = NULL;
+  size_t sz;
+  size_t i = 0;
+  if (argc == 1) {
+    while (getline(&line, &sz, stdin) > 1) {
+      lines = realloc(lines, (i + 1) * sizeof(*lines));
+      lines[i] = line;
+      line = NULL;
+      i++;
+    }
+    free(line);
+    sortData(lines, i);
+    for (size_t j = 0; j < i; j++) {
+      printf("%s", lines[j]);
+      free(lines[j]);
+    }
+    free(lines);
+  }
+  else {
+    for (int j = 1; j < argc; j++) {
+      FILE * f = fopen(argv[j], "r");
+      if (f == NULL) {
+        fprintf(stderr, "Can't find the file!");
+        exit(EXIT_FAILURE);
+      }
+      i = 0;
+      while (getline(&line, &sz, f) >= 0) {
+        lines = realloc(lines, (i + 1) * sizeof(*lines));
+        lines[i] = line;
+        line = NULL;
+        i++;
+      }
+      free(line);
+      line = NULL;
+      if (i == 0) {
+        fprintf(stderr, "No line!");
+        exit(EXIT_FAILURE);
+      }
+      sortData(lines, i);
+      for (size_t k = 0; k < i; k++) {
+        printf("%s", lines[k]);
+        free(lines[k]);
+      }
+      free(lines);
+      lines = NULL;
+      fclose(f);
+    }
+  }
   return EXIT_SUCCESS;
 }
