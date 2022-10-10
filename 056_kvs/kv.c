e24c92bba86316dd32077852a90c68ee7eb70e3a
jc977 <jc977@duke.edu>
2022-10-10 17:08:28 -0400

    assignment 056 1st try

--- 056_kvs/kv.c
+++ 056_kvs/kv.c
@@ -1,22 +1,68 @@
+#include "kv.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "kv.h"
-
-
 
 kvarray_t * readKVs(const char * fname) {
-  //WRITE ME
+  FILE * f = fopen(fname, "r");
+  kvarray_t * kvarr = malloc(sizeof(*kvarr));
+  kvarr->arr = NULL;
+  kvarr->len = 0;
+  char * line = NULL;
+  size_t sz = 0;
+  while (getline(&line, &sz, f) > 0) {
+    if (*line == '\n') {
+      continue;
+    }
+    kvarr->len++;
+    kvarr->arr = realloc(kvarr->arr, kvarr->len * sizeof(*kvarr->arr));
+    myReadLine(line, &kvarr->arr[kvarr->len - 1]);
+  }
+  fclose(f);
+  free(line);
+  return kvarr;
+}
+
+void myReadLine(char * line, kvpair_t * kvtarget) {
+  kvtarget->key = NULL;
+  kvtarget->value = NULL;
+  readKeyorVal(&line, &kvtarget->key, '=');
+  readKeyorVal(&line, &kvtarget->value, '\n');
+}
+
+void readKeyorVal(char ** line, char ** target, char sign) {
+  int len = 0;
+  while (**line != sign) {
+    *target = realloc(*target, ++len * sizeof(**target));
+    (*target)[len - 1] = **line;
+    (*line)++;
+  }
+  *target = realloc(*target, ++len * sizeof(*target));
+  (*target)[len - 1] = '\0';
+  (*line)++;
 }
 
 void freeKVs(kvarray_t * pairs) {
-  //WRITE ME
+  for (int i = 0; i < pairs->len; i++) {
+    free(pairs->arr[i].key);
+    free(pairs->arr[i].value);
+  }
+  free(pairs->arr);
+  free(pairs);
 }
 
 void printKVs(kvarray_t * pairs) {
-  //WRITE ME
+  for (int i = 0; i < pairs->len; i++) {
+    printf("key = '%s' value = '%s'\n", pairs->arr[i].key, pairs->arr[i].value);
+  }
 }
 
 char * lookupValue(kvarray_t * pairs, const char * key) {
-  //WRITE ME
+  for (int i = 0; i < pairs->len; i++) {
+    if (strcmp(pairs->arr[i].key, key) == 0) {
+      return pairs->arr[i].value;
+    }
+  }
+  return NULL;
 }
--- 056_kvs/kv.h
+++ 056_kvs/kv.h
@@ -1,22 +1,23 @@
 #ifndef __KV_H__
 #define __KV_H__
 
-
-struct _kvpair_t { 
-  //DEFINE ME!
-
+struct _kvpair_t {
+  char * key;
+  char * value;
 };
 typedef struct _kvpair_t kvpair_t;
 
-struct _kvarray_t { 
-  //DEFINE ME!
-
-
+struct _kvarray_t {
+  kvpair_t * arr;
+  int len;
 };
 typedef struct _kvarray_t kvarray_t;
 
-
 kvarray_t * readKVs(const char * fname);
+
+void myReadLine(char * line, kvpair_t * kvarr);
+
+void readKeyorVal(char ** line, char ** target, char sign);
 
 void freeKVs(kvarray_t * pairs);
 
