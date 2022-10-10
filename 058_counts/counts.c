341230eaecf4b16c6e3458c3c516b99e1f5f981b
jc977 <jc977@duke.edu>
2022-10-10 17:47:44 -0400

    assignment 058 1st try

--- 058_counts/counts.c
+++ 058_counts/counts.c
@@ -1,17 +1,49 @@
+#include "counts.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "counts.h"
+
 counts_t * createCounts(void) {
-  //WRITE ME
+  counts_t * c = malloc(sizeof(*c));
+  c->arr = NULL;
+  c->len = 0;
+  c->unknownum = 0;
+  return c;
 }
+
 void addCount(counts_t * c, const char * name) {
-  //WRITE ME
+  if (name == NULL) {
+    c->unknownum++;
+    return;
+  }
+  for (int i = 0; i < c->len; i++) {
+    if (strcmp(name, c->arr[i].stringName) == 0) {
+      c->arr[i].count++;
+      return;
+    }
+  }
+  c->len++;
+  c->arr = realloc(c->arr, c->len * sizeof(*c->arr));
+  c->arr[c->len - 1].stringName =
+      malloc((strlen(name) + 1) * sizeof(*c->arr[c->len - 1].stringName));
+  strcpy(c->arr[c->len - 1].stringName, name);
+  c->arr[c->len - 1].count = 1;
 }
 void printCounts(counts_t * c, FILE * outFile) {
-  //WRITE ME
+  for (int i = 0; i < c->len; i++) {
+    fprintf(outFile, "%s: %d\n", c->arr[i].stringName, c->arr[i].count);
+  }
+  if (c->unknownum > 0) {
+    fprintf(outFile, "<unknown> : %d\n", c->unknownum);
+  }
+  fclose(outFile);
 }
 
 void freeCounts(counts_t * c) {
-  //WRITE ME
+  for (int i = 0; i < c->len; i++) {
+    free(c->arr[i].stringName);
+  }
+  free(c->arr);
+  free(c);
 }
--- 058_counts/counts.h
+++ 058_counts/counts.h
@@ -1,13 +1,18 @@
 #ifndef __COUNTS_H__
 #define __COUNTS_H__
-struct _one_count_t {
-  //DEFINE ME
 
+#include <stdio.h>
+
+struct _one_count_t {
+  char * stringName;
+  int count;
 };
 typedef struct _one_count_t one_count_t;
 
 struct _counts_t {
-  //DEFINE ME
+  one_count_t * arr;
+  int len;
+  int unknownum;
 };
 typedef struct _counts_t counts_t;
 
