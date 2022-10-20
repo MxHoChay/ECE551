521f1dcb77d5aeb413385d52dbcc47c79853488b
jc977 <jc977@duke.edu>
2022-10-20 13:35:48 -0400

    eval2 step4

--- 060_eval2/story-step4.c
+++ 060_eval2/story-step4.c
@@ -11,11 +11,14 @@ int main(int argc, char ** argv) {
   FILE * fword = myOpen(argv[argc - 2]);
   FILE * fstory = myOpen(argv[argc - 1]);
   catarray_t * cats = createCat(fword);
-  if (argc == 4 && strcmp(argv[1], "-n") == 0) {
+  if (argc == 4 && strcmp("-n", argv[1]) == 0) {
     createStory(fstory, cats, false);
   }
-  else {
+  else if (argc == 3) {
     createStory(fstory, cats, true);
+  }
+  else {
+    exitErr("Invalid input format!");
   }
   fclose(fword);
   fclose(fstory);
