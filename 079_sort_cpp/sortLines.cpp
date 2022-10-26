1c5d3d411279186088a84b744c9eb6a8fd6532c0
jc977 <jc977@duke.edu>
2022-10-26 18:41:56 -0400

    assignment 079 1st try

---
+++ 079_sort_cpp/sortLines.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+int main(int argc, char ** argv) {
+  if (argc == 1) {
+    std::vector<std::string> arr;
+    std::string str;
+    while (true) {
+      std::getline(std::cin, str);
+      if (str == "") {
+        break;
+      }
+      arr.push_back(str);
+    }
+    std::sort(arr.begin(), arr.end());
+    std::vector<std::string>::iterator it = arr.begin();
+    while (it != arr.end()) {
+      std::cout << *it << std::endl;
+      ++it;
+    }
+  }
+  else {
+    for (int i = 1; i < argc; i++) {
+      std::vector<std::string> arr;
+      std::string str;
+      std::ifstream file;
+      file.open(argv[i], std::ifstream::in);
+      while (std::getline(file, str)) {
+        arr.push_back(str);
+      }
+      std::sort(arr.begin(), arr.end());
+      std::vector<std::string>::iterator it = arr.begin();
+      while (it != arr.end()) {
+        std::cout << *it << std::endl;
+        ++it;
+      }
+    }
+  }
+  return EXIT_SUCCESS;
+}
