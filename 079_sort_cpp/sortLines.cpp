588084d672b2ac8455763a24d09f320415cb49b1
jc977 <jc977@duke.edu>
2022-10-26 20:16:36 -0400

    assignment 079 2nd try

--- 079_sort_cpp/sortLines.cpp
+++ 079_sort_cpp/sortLines.cpp
@@ -28,6 +28,10 @@ int main(int argc, char ** argv) {
       std::string str;
       std::ifstream file;
       file.open(argv[i], std::ifstream::in);
+      if (!file.is_open()) {
+        std::cerr << "Invalid file name!" << std::endl;
+        exit(EXIT_FAILURE);
+      }
       while (std::getline(file, str)) {
         arr.push_back(str);
       }
