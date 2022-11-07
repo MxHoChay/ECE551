5b1d752b53dcd3b96a39b403d2a2bcb8a7c743ba
jc977 <jc977@duke.edu>
2022-11-06 21:03:53 -0500

    assignment 092 1st try

--- 092_lls/ll.hpp
+++ 092_lls/ll.hpp
@@ -7,5 +7,124 @@
 #include <exception>
 
 //YOUR CODE GOES HERE
+template<typename T>
+class LinkedList {
+  class Node {
+   public:
+    T data;
+    Node * next;
+    Node * prev;
+    Node() : next(NULL), prev(NULL) {}
+    Node(T d, Node * n, Node * p) : data(d), next(n), prev(p) {}
+  };
+  Node * head;
+  Node * tail;
+  int size;
+
+ public:
+  LinkedList() : head(new Node()), tail(new Node()), size(0) {
+    head->next = tail;
+    tail->prev = head;
+  }
+
+  LinkedList(const LinkedList & rhs) :
+      head(new Node()), tail(new Node()), size(rhs.size) {
+    head->next = tail;
+    tail->prev = head;
+    for (Node * i = rhs.head->next; i != rhs.tail; i = i->next) {
+      addBack(i->data);
+    }
+  }
+
+  LinkedList & operator=(const LinkedList & rhs) {
+    if (this == &rhs) {
+      return *this;
+    }
+    myclear();
+    head = new Node();
+    tail = new Node();
+    size = rhs.size;
+    head->nexy = tail;
+    tail->prev = head;
+    for (Node * i = rhs.head->next; i != rhs.tail; i = i->next) {
+      addBack(i->data);
+    }
+  }
+
+  ~LinkedList() { myclear(); }
+
+  void myclear() {
+    Node * i = head->next;
+    while (i != tail) {
+      Node * temp = i->next;
+      delete i;
+      i = temp;
+    }
+    delete head;
+    delete tail;
+  }
+
+  void addFront(const T & item) {
+    Node * node = new Node(item, head->next, head);
+    head->next->prev = node;
+    head->next = node;
+    size++;
+  }
+
+  void addBack(const T & item) {
+    Node * node = new Node(item, tail, tail->prev);
+    tail->prev->next = node;
+    tail->prev = node;
+    size++;
+  }
+
+  bool remove(const T & item) {
+    for (Node * i = head->next; i != tail; i = i->next) {
+      if (i->data == item) {
+        i->prev->next = i->next;
+        i->next->prev = i->prev;
+        delete i;
+        size--;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  T & operator[](int index) throw(std::exception) {
+    if (index < 0 || index >= size) {
+      throw std::exception();
+    }
+    Node * node = head;
+    for (int i = 0; i <= index; i++) {
+      node = node->next;
+    }
+    return node->data;
+  }
+
+  const T & operator[](int index) const throw(std::exception) {
+    if (index < 0 || index >= size) {
+      throw std::exception();
+    }
+    Node * node = head;
+    for (int i = 0; i <= index; i++) {
+      node = node->next;
+    }
+    return node->data;
+  }
+
+  int find(const T & item) const {
+    int index = 0;
+    for (Node * i = head->next; i != tail; i = i->next) {
+      if (i->data == item) {
+        return index;
+      }
+      index++;
+    }
+    return -1;
+  }
+
+  int getSize() const { return size; }
+};
 
 #endif
