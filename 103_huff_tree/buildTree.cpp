eb4a43e6f6a466a90848996580f38f9b1cf1827c
jc977 <jc977@duke.edu>
2022-11-21 14:27:37 -0500

    assignment 103 1st try

--- 103_huff_tree/buildTree.cpp
+++ 103_huff_tree/buildTree.cpp
@@ -1,5 +1,20 @@
 #include "node.h"
 
-Node *  buildTree(uint64_t * counts) {
+Node * buildTree(uint64_t * counts) {
   //WRITE ME!
-}  
+  priority_queue_t mytree;
+  for (size_t i = 0; i < 257; ++i) {
+    if (counts[i] != 0) {
+      mytree.push(new Node(i, counts[i]));
+    }
+  }
+  while (mytree.size() > 1) {
+    Node * first = mytree.top();
+    mytree.pop();
+    Node * second = mytree.top();
+    mytree.pop();
+    Node * newNode = new Node(first, second);
+    mytree.push(newNode);
+  }
+  return mytree.top();
+}
